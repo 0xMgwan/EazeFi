9789ee0c2bb31578c16fbf9287974816a610ba76
0xMgwan <davidmachuche@gmail.com>
2025-03-30 17:03:12 +0400

    Fix ToastContainer reference error in Wallet component

--- client/src/components/wallet/Wallet.js
+++ client/src/components/wallet/Wallet.js
@@ -9,6 +9,8 @@ import DebugBalanceChecker from './DebugBalanceChecker';
 import DirectBalanceDisplay from './DirectBalanceDisplay';
 import TopBalanceDisplay from './TopBalanceDisplay';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { showSuccess, showError, showInfo, showLoading, updateToast } from '../../utils/notificationUtils';
 
 const Wallet = () => {
